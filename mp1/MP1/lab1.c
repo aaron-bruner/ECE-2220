aa9ca43f82e6998d1f8cf97a9f7c25a9cb1d862b
Aaron Joseph Bruner <ajbrune@g.clemson.edu>
2020-09-15 17:41:09 -0400

    Finished Lab1

--- mp1/MP1/lab1.c
+++ mp1/MP1/lab1.c
@@ -1,10 +1,8 @@
 /* lab1.c
  * Aaron Bruner
  * ajbrune
- * ECE 2220, Fall 2019
+ * ECE 2220, Fall 2020
  * MP1
- *
- * NOTE:  You must update all of the following comments!
  * 
  * Purpose:  The purpose of machine problem one is to practice our C programming skills in handling inputs from the
  *           terminal along with processing outputs. Also, we are to use conditional statements and loops to get the
@@ -23,28 +21,12 @@
  *        bug would be if someone compiles the code on a non standard machine which may produce errors that were not
  *        initially produced.
  *
- * See the ECE programming guide for requirements
- *
- * Are you unhappy with the way this code is formatted?  You can easily
- * reformat (and automatically indent) your code using the astyle 
- * command.  If it is not installed use the Ubuntu Software Center to 
- * install astyle.  Then in a terminal on the command line do
- *     astyle --style=kr lab1.c
- *
- * See "man astyle" for different styles.  Replace "kr" with one of
- * ansi, java, gnu, linux to see different options.  Or, set up your
- * own style.
- *
- * To create a nicely formated PDF file for printing install the enscript 
- * command.  To create a PDF for "file.c" in landscape with 2 columns do:
- *     enscript file.c -G2rE -o - | ps2pdf - file.pdf
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-//notice there is no semi-colon at the end of a #define
 #define MAXLINE 100
 #define MINNUM 10
 #define MAXNUM 100
@@ -53,69 +35,76 @@
 #define MINKEY 1
 #define MAXKEY 5
 
-
+/* The main function starts off by initializing our two variables msgSize and keyValue
+ * Once we initialize these values we can start to gather our messages from the user
+ * The infinite while loop will continue to run until the -2 value is read in; or
+ * if a value is read in that does not meet the specifications.
+ *
+ * inputs none
+ *
+ * outputs the exit code; good exit is 0 bad is 1
+ */
 int main() {
-    int isPrime(int number, int keyValue);                     // Function Headers //
-    void sendMessage(int numOfPrimes, int id);   //                  //
-
-    char line[MAXLINE];         // the entire message
-    int msgSize;                // amount of values per message
-    int keyValue, id = 0;           // key value is our prime checker and id the is last value in the array with 2 or 4 prime numbers
-    int numOfPrimes;
-    int temp;
-    int counter = 0;
+    int isPrime(int number);
+    void sendMessage(int msgSize, int key, int arr[]);
+    char line[MAXLINE];
+    int msgSize;
+    int keyValue;
+    int message[MAXLINE];
+    int counter = 1; // Used to determine what message # we're on
 
     for (int j = 0; j < 2; j++) {
         if (j == 0) {
+            printf(">> Please enter a message size: ");
             fgets(line, MAXLINE, stdin);
             sscanf(line, "%d", &msgSize);
-            msgSize = line[0]-'0';
-            if (msgSize < MINSIZE || msgSize >= MAXSIZE) {
-                printf("Error 1: The message size is not valid...\n");
+            msgSize = line[0]-'0';  // Convert from ASCII character to integer
+            if (msgSize < MINSIZE || msgSize >= MAXSIZE) { // Check to see if the msgSize is not between 6 and 12
+                printf("\n>> The message size is not valid...\n");
                 exit(1);
             }
         }
         else {
+            printf("\n>> Please enter a key value: ");
             fgets(line, MAXLINE, stdin);
             sscanf(line, "%d", &keyValue);
-            keyValue = line[0]-'0';
-            if (keyValue <= MINKEY || keyValue >= MAXKEY) {
-                printf("Error 2: The key value is not valid...\n");
+            keyValue = line[0]-'0'; // Convert from ASCII character to integer
+            if (keyValue <= MINKEY || keyValue >= MAXKEY) { // Check to see if the keyValue is not between 2 and 4
+                printf("\n>> The key value is not valid...\n");
                 exit(1);
             }
         }
     }
-
-    while (counter < 100) {
-
-    for (int r = 0; r < msgSize; r++) {
-        fgets(line, MAXLINE, stdin);
-        sscanf(line, "%d", &temp);
-        temp = line[0]-'0';
-        if (temp == 0) {
-            printf(">> No more message received. Program exit.\n\n\n");
-            exit(1);
+    while (1) {
+        printf("\n>> Please enter message number %d: \n", counter);
+        for (int e = 0; e < msgSize; e++){ // Start to fill up our integers array with msgSize amount of values
+            char integers[MAXLINE];
+            fgets(integers,MAXLINE,stdin);
+            sscanf(integers,"%d",message+e);
+            if (message[e] == -2) { // As soon as we see -2 we exit
+                printf("\n>> Agent Center is not safe now. Go find a safe place. Program exit.\n\n\n");
+                exit(1);
+            }
+            if(message[e] == 0 || feof(stdin)) { // Make sure we don't keep getting values after the end
+                printf("\n>> Failed to gather valid input... Program exit.\n");
+                exit(1);
+            }
         }
-        /*if (temp <= MINNUM || temp >= MAXNUM)
-            printf(">> Corrupted Message.\n");*/
-        numOfPrimes = 0; numOfPrimes += isPrime(temp, keyValue);
-        if (r == msgSize-1) {
-            id = temp-'0';
-            sendMessage(numOfPrimes, id);
-        }
+        sendMessage(msgSize, keyValue, message);
+        counter++;
     }
-    counter++;
-    }
-
-
-    printf("\nMsgsize is set to %d.\n", msgSize);
-    printf("\nKeyValue is set to %d.\n", keyValue);
-
     exit(0);
+
 }
 
-int isPrime(int number, int keyValue) {
-    number -= keyValue;
+/* The isPrime function determines if the input value is a prime number
+ *
+ * inputs a single positive integer value
+ *
+ * outputs a 1 for true it is a prime number or 0 for false it is not a prime number
+ */
+int isPrime(int number) {
+
     if (number <= 1) return 0; // 0 implies non-prime
     if (number % 2 == 0 && number > 2) return 0;
     for(int i = 3; i < number / 2; i+= 2) {
@@ -125,12 +114,30 @@ int isPrime(int number, int keyValue) {
     return 1; // 1 implies prime number
 }
 
-void sendMessage (int numOfPrimes, int id) {
-
+/* The sendMessage function is what determines what message should
+ * be displayed based on the amount of primes values that we decoded
+ *
+ * inputs an integer msgSize so we can loop through exactly how many
+ * values we have in our array. The integer key which is used to check
+ * our prime numbers. And lastly the message array which is also used for
+ * the prime number array.
+ *
+ * output void
+ */
+void sendMessage (int msgSize, int key, int message[]) {
+    int numOfPrimes = 0; // Make sure that we don't keep any old values
+    int id = 0; // This is for when we have a case 2 or 4 and we need the agent's id (the last value in the message)
+    for (int q = 0; q < msgSize; q++) {
+        if(message[q] <= MINNUM || message[q] >= MAXNUM) { // Ensure that our message is the correct size (not < our msgSize and not > our msgSize)
+            numOfPrimes = MAXNUM; // Another way of setting off a flag for the switch
+        }
+        if (isPrime(message[q]-key) && numOfPrimes != MAXNUM) // Our prime number counter
+            numOfPrimes++;
+    }
+    if (numOfPrimes == 2 || numOfPrimes == 4) { // If we need the agent ID we get it here
+        id = message[msgSize-1];
+    }
     switch (numOfPrimes) {
-        case -2:
-            printf("\n>> Agent Center is not safe now. Go find a safe place. Program exit.\n\n\n");
-            exit(1);
         case 1:
             printf(">> Msg:  I'm safe, all good.\n");
             break;
@@ -145,6 +152,9 @@ void sendMessage (int numOfPrimes, int id) {
             break;
         case 5:
             printf(">> Msg:  No Content.    \n");
+            break;
+        case 100:
+            printf(">> Corrupted Message.\n");
             break;
         default:
             break;
