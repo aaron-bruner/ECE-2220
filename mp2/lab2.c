5a5f0003d0eefa7eb7a38185ed7da82e0a78f320
Aaron Joseph Bruner <ajbrune@g.clemson.edu>
2020-09-27 21:16:07 -0400

    Lab 2

--- mp2/lab2.c
+++ mp2/lab2.c
@@ -1,10 +1,10 @@
 /* lab2.c
- * Your Name    <<-- you must change to your name and id
- * Clemson ID
- * ECE 222, Fall 2020
+ * Aaron Bruner (ajbrune)   <<-- you must change to your name and id
+ * C16480080
+ * ECE 222-001, Fall 2020
  * MP2 (2020)
  *
- * Purpose:  A template for MP2 -- replace
+ * Purpose:  Utilize bitwise operations on data to perform data corrections.
  *
  * Assumptions:
  *  #1:  The menu driven input was provided and must be used exactly
@@ -61,7 +61,7 @@
 
 // function prototypes
 void encode(unsigned char first_letter, unsigned char second_letter,
-        unsigned char third_letter);
+            unsigned char third_letter);
 void decode(unsigned int codeword);
 
 int main()
@@ -84,7 +84,7 @@ int main()
         } else if (items == 2 && strcmp(command, "enc") == 0) {
             // encoding
             if (strlen(inputcs) != 3 || !isprint(inputcs[0]) ||
-                    !isprint(inputcs[1]) || !isprint(inputcs[2])) {
+                !isprint(inputcs[1]) || !isprint(inputcs[2])) {
                 printf("Invalid input to encoder: %s\n", inputcs);
             } else {
                 encode(inputcs[0], inputcs[1], inputcs[2]);
@@ -105,7 +105,7 @@ int main()
                 if (invalid) {
                     printf("Invalid decoder digits: %s\n", inputcs);
                 } else if (strlen(inputcs) == 8 && inputcs[0] != '1'
-                        && inputcs[0] != '0') {
+                           && inputcs[0] != '0') {
                     printf("Invalid decoder leading digit: %s\n", inputcs);
                 } else {
                     decode(codeword);
@@ -119,42 +119,158 @@ int main()
     return 0;
 }
 
-/* encode: calculates parity bits and prints codeword
+/* The encode function does arithmetic with parity bits and prints the codeword.
  *
- * input: three ASCII characters
- * assumptions: input is valid
+ * inputs The three inputs are unsigned characters; they are ASCII characters.
  *
- * Example: if input letters are is 'C', 'U', and 't'
+ * assumptions We have to assume that the user is going to input exactly
+ *             three true ASCII characters
+ *
+ * Example: if input letters are is 'A', '@', and '@'
  * the final print must be:
- *  ---01110 10001010 01010100 10011100
- *  Codeword: 0x0E8A549C
+ * ---01000 00001000 00000100 00000100
+ * Codeword: 0x08080404
  */
 void encode(unsigned char first_letter, unsigned char second_letter,
-        unsigned char third_letter)
+            unsigned char third_letter)
 {
     // you must construct the codeword
     unsigned int codeword = 0;
-    int i = -1;  // dummy variable
 
     printf("%9s%9c%9c%9c\n", "Encoding:", third_letter, second_letter, first_letter);
     printf(" 0x    00%9x%9x%9x\n", third_letter, second_letter, first_letter);
 
-    //
-    // ADD your code here
-    //
+    int third_let = third_letter << 16;
+    int second_let = second_letter << 8;
+    int total = third_let + second_let + first_letter; //determine the binary sum of characters
+    int temptotal = total; //placeholder variable for later use
+    int firstShift, secondShift, thirdShift;
+    int P1, P2, P4, P8, P16; //parity bits
+    int p1, p2, p4, p8, p16;
+    int count1, count2, count4, count8, count16;
+    int parity1, parity2, parity4, parity8, parity16;
+    int i; // dummy variable
 
-    // print the information word in binary form with spaces
+    //saves more digits, clears bits, and adds back saved values
+    //shift 1
+    firstShift = 0x7FF & total;
+    total <<= 5;
+    total &= ~0xFFFF;
+    //shift 2
+    secondShift = 0xF & firstShift;
+    firstShift <<= 4;
+    firstShift &= ~0xFF;
+    total = total + firstShift;
+    //shift 3
+    thirdShift = 0x1 & secondShift;
+    secondShift <<= 3;
+    secondShift &= ~0xF;
+    total = total + secondShift;
 
-    // print the parity bits, one bit per line.  Do not change
-    // the format, but you must change the dummy variable i
-    // to match your design
-    printf("P1 : %d\n", i);
-    printf("P2 : %d\n", i);
-    printf("P4 : %d\n", i);
-    printf("P8 : %d\n", i);
-    printf("P16: %d\n", i);
+    thirdShift <<= 2;
+    thirdShift &= ~0x3;
+    total += thirdShift;
+
+    /* Determine the parity bits */
+
+    // Determine the first parity bit
+    p1 = 0x15555555 & total;
+    count1 = 0;
+    while(p1 > 0) {
+        if((p1 & 1) == 1)
+            count1++;
+        p1 >>= 1;
+    }
+    if((count1 % 2) == 1)
+        P1 = 1;
+    else
+        P1 = 0;
+
+    // Determine the second parity bit
+    p2 = 0x6666666 & total;
+    count2 = 0;
+    while(p2 > 0) {
+        if((p2 & 1) == 1)
+            count2++;
+        p2 >>= 1;
+    }
+    if((count2 % 2) == 1)
+        P2 = 1;
+    else
+        P2 = 0;
+
+    // Determine the third parity bit
+    p4 = 0x18787878 & total;
+    count4 = 0;
+    while(p4 > 0) {
+        if((p4 & 1) == 1)
+            count4++;
+        p4 >>= 1;
+    }
+    if((count4 % 2) == 1)
+        P4 = 1;
+    else
+        P4 = 0;
+
+    // Determine the fourth parity bit
+    p8 = 0x1F807F80 & total;
+    count8 = 0;
+    while(p8 > 0) {
+        if((p8 & 1) == 1)
+            count8++;
+        p8 >>= 1;
+    }
+    if((count8 % 2) == 1)
+        P8 = 1;
+    else
+        P8 = 0;
+
+    // Determine the fifth parity bit
+    p16 = 0x1FFF8000 & total;
+    count16 = 0;
+    while(p16 > 0) {
+        if((p16 & 1) == 1)
+            count16++;
+        p16 >>= 1;
+    }
+    if((count16 % 2) == 1)
+        P16 = 1;
+    else
+        P16 = 0;
+
+    //finds actual values of parity bits for adding
+    parity16 = P16 * 0x8000;
+    parity8 = P8 * 0x80;
+    parity4 = P4 * 0x8;
+    parity2 = P2 * 0x2;
+    parity1 = P1 * 0x1;
+
+    codeword = total + parity16 + parity8 + parity4 + parity2 + parity1;
+
+    //prints original information word in binary
+    printf(" -------- ");
+    for(i=23; i>=0; i--) {
+        printf("%d", (temptotal & (1 << i)) >> i);
+        if((i == 8) || (i == 16))
+            printf(" ");
+    }
+    printf("\n");
+
+    // prints the parity bits, one bit per line.  Do not change
+    printf("P1 : %d\n", P1);
+    printf("P2 : %d\n", P2);
+    printf("P4 : %d\n", P4);
+    printf("P8 : %d\n", P8);
+    printf("P16: %d\n", P16);
 
     // print the codeword bits in binary form with spaces
+    printf(" ---");
+    for(i=28; i>=0; i--) {
+        printf("%d", (codeword & (1 << i)) >> i);
+        if((i == 8) || (i == 16) || (i == 24))
+            printf(" ");
+    }
+    printf("\n");
 
     // print the codeword in hex format
     printf(" Codeword: 0x%.8X\n", codeword);
@@ -190,30 +306,142 @@ void encode(unsigned char first_letter, unsigned char second_letter,
 void decode(unsigned int codeword)
 {
     // you must determine these values:
-    unsigned int info_word = 0;
-    unsigned char first_letter = 0x42;
-    unsigned char second_letter = 0x61;
-    unsigned char third_letter = 0x64;
-    int bit_error_location = 21;
+    int codeword1 = codeword & ~0x808B; //codeword without parity bits
+    int P1, P2, P4, P8, P16;
+    int E1, E2, E4, E8, E16; //error values
+    int errorBit1, errorBit2, errorBit4, errorBit8, errorBit16;
+    int firstShift, secondShift, thirdShift;
+    int p1, p2, p4, p8, p16;
+    int count1, count2, count4, count8, count16;
+    unsigned int info_word;
+    int bit_error_location;
+    int error_value, i;
+    unsigned char first_letter;
+    unsigned char second_letter;
+    unsigned char third_letter;
 
-    // dummy variable
-    int i = -1;
+    //recalculates parity bits from codeword and assigns error values
+    p1 = 0x15555555 & codeword1;
+    count1 = 0;
+    while(p1 > 0) {
+        if((p1 & 1) == 1)
+            count1++;
+        p1 = p1 >> 1;
+    }
+    if((count1 % 2) == 1)
+        P1 = 1;
+    else P1 = 0;
+    if(P1 == (0x1 & codeword)) {
+        E1 = 0;
+    }else E1 = 1;
+
+    p2 = 0x6666666 & codeword1;
+    count2 = 0;
+    while(p2 > 0) {
+        if((p2 & 1) == 1)
+            count2++;
+        p2 >>= 1;
+    }
+    if((count2 % 2) == 1)
+        P2 = 1;
+    else P2 = 0;
+    if(P2*2 == (0x2 & codeword)) {
+        E2 = 0;
+    }else E2 = 1;
+
+    p4 = 0x18787878 & codeword1;
+    count4 = 0;
+    while(p4 > 0) {
+        if((p4 & 1) == 1)
+            count4++;
+        p4 >>= 1;
+    }
+    if((count4 % 2) == 1)
+        P4 = 1;
+    else P4 = 0;
+    if(P4*8 == (0x8 & codeword)) {
+        E4 = 0;
+    }
+    else E4 = 1;
+
+    p8 = 0x1F807F80 & codeword1;
+    count8 = 0;
+    while(p8 > 0) {
+        if((p8 & 1) == 1)
+            count8++;
+        p8 >>= 1;
+    }
+    if((count8 % 2) == 1)
+        P8 = 1;
+    else P8 = 0;
+    if(P8*128 == (0x80 & codeword)) {
+        E8 = 0;
+    }
+    else E8 = 1;
+
+    p16 = 0x1FFF8000 & codeword1;
+    count16 = 0;
+    while(p16 > 0) {
+        if((p16 & 1) == 1)
+            count16++;
+        p16 >>= 1;
+    }
+    if((count16 % 2) == 1)
+        P16 = 1;
+    else P16 = 0;
+    if(P16*32768 == (0x8000 & codeword)) {
+        E16 = 0;
+    }
+    else E16 = 1;
+
+    //finds int values of error to add for error location
+    errorBit16 = E16 * 0x10;
+    errorBit8 = E8 * 0x8;
+    errorBit4 = E4 * 0x4;
+    errorBit2 = E2 * 0x2;
+    errorBit1 = E1 * 0x1;
 
     printf("Decoding: 0x%.8X\n", codeword);
 
-    //
-    // ADD your code here to fix a bit error if one exists
-    // and determine the three characters
-    //
+    bit_error_location = errorBit16 + errorBit8 + errorBit4 + errorBit2 + errorBit1;
 
-    // print the error location bits, one bit per line.  Do not change
-    // the format, but you must change the dummy variable i
-    // to match your design
-    printf("E1 : %d\n", i);
-    printf("E2 : %d\n", i);
-    printf("E4 : %d\n", i);
-    printf("E8 : %d\n", i);
-    printf("E16: %d\n", i);
+    //finds int values bit error location
+    error_value = 1;
+    for(i = 0; i < bit_error_location-1; i++) {
+        error_value *= 2;
+    }
+
+    //corrects erroneous bit
+    if((error_value & codeword) == error_value)
+        codeword -= error_value;
+    else
+        codeword += error_value;
+
+    //creates info_word from corrected codeword
+    info_word = codeword >> 2;
+    firstShift = 0x1 & info_word;
+    info_word >>= 2;
+    secondShift = 0x7 & info_word;
+    secondShift <<= 1;
+    info_word >>= 4;
+    thirdShift = 0x7F & info_word;
+    thirdShift <<= 4;
+    info_word >>= 8;
+    info_word <<= 11;
+    info_word = info_word + firstShift + secondShift + thirdShift;
+
+    //converts info_word into three ASCII codes for printing
+    first_letter = info_word & 0xFF;
+    second_letter = (info_word & 0xFF00) >> 8;
+    third_letter = (info_word & 0xFF0000) >> 16;
+
+
+    // prints the error location bits, one bit per line.  Do not change
+    printf("E1 : %d\n", E1);
+    printf("E2 : %d\n", E2);
+    printf("E4 : %d\n", E4);
+    printf("E8 : %d\n", E8);
+    printf("E16: %d\n", E16);
 
     // here is the required format for the prints.  Do not
     // change the format but update the variables to match
@@ -225,9 +453,17 @@ void decode(unsigned int codeword)
     } else
         printf(" Decoding failure: %d\n", bit_error_location);
 
-    // you must print the info_word in binary format
+    //prints the info_word in binary format
+    info_word &= 0xFFFFFF;
+    printf(" -------- ");
+    for(i=23; i>=0; i--) {
+        printf("%d", (info_word & (1 << i)) >> i);
+        if((i == 8) || (i == 16))
+            printf(" ");
+    }
+    printf("\n");
 
-    // print the information word in hex:
+    // prints the information word in hex:
     printf(" Information Word: 0x%.6X", info_word);
 
     // You must convert the info_word into three characters for printing
